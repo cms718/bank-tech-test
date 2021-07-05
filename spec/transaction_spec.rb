df5037097cddfbcff79d595c129d7f6bd99646af
Charlie Slater <charliemslater1701@gmail.com>
2021-07-05 12:51:51 +0100

    Refactor tests

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,17 +1,19 @@
 require 'transaction'
 
 describe Transaction do
+  before(:each) do
+    @transaction = Transaction.new(1000, "debit")    
+  end
+  
   describe '.amount' do 
     it 'stores the value of the transaction' do
-      transaction = Transaction.new(1000, "debit")
-      expect(transaction.amount).to eq(1000)
+      expect(@transaction.amount).to eq(1000)
     end
   end
   
   describe '.type' do
     it 'stores either credit or debit' do
-      transaction = Transaction.new(1000, "debit")
-      expect(transaction.type).to eq("debit")
+      expect(@transaction.type).to eq("debit")
     end
   end
 end
