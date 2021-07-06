3a3ef29f93f773ad0dc92bb30c1e6e18b0bf2055
Charlie Slater <charliemslater1701@gmail.com>
2021-07-06 10:50:52 +0100

    Refactored tests

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -9,18 +9,22 @@ describe BankAccount do
       end
     end
   end
+
   describe ".deposit" do
-    it "adds a new transaction to transactions" do
-      subject.deposit(1000)
-      expect(subject.transactions.length).to eq(1)
-    end
-    it "creates a new credit transaction" do
+    
+    before(:each) do
       transaction = class_double("Transaction")
       bank_account = BankAccount.new(transaction)
-      expect(transaction).to receive(:create).with(1000)
-      bank_account.deposit(1000)
+      mock_new_transaction = double("Transaction", :value => 1000)
+      allow(transaction).to receive(:create).with(1000).and_return(mock_new_transaction)
+    end
+    
+    it "adds a new transaction to transactions" do
+      subject.deposit(1000)
+      expect(subject.transactions.first.value).to eq(1000)
     end
   end
+
   describe ".withdraw" do
     it "adds a new debit transaction" do
       transaction = class_double("Transaction")
