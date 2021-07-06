7a848650e0edfec57cce766a08daa7295ec19811
Charlie Slater <charliemslater1701@gmail.com>
2021-07-06 13:14:49 +0100

    Added statement printer for 1 statement

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,11 +1,8 @@
 class StatementPrinter
-  def run(statement)
-    puts statement_header
-  end
-
-  private
-
-  def statement_header
-    "date || credit || debit || balance"
+  def self.run(statement)
+    puts "date || credit || debit || balance"
+    if statement.any?
+      puts "#{statement[0][:date]} || #{statement[0][:value]} || || #{statement[0][:balance]}"
+    end
   end
 end
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -4,8 +4,19 @@ describe StatementPrinter do
   describe '#run' do
     context "when not given empty statement" do
       it "displays only the headers" do
-        expect { subject.run([]) }.to output("date || credit || debit || balance\n").to_stdout
+        expect { StatementPrinter.run([]) }.to output("date || credit || debit || balance\n").to_stdout
       end
+    end
+
+    it "prints the formatted statement with 1 transaction" do
+      statement = [{ 
+        date: "01/01/2000", 
+        type: "credit",
+        value: 1000.00, 
+        balance: 1000.00 
+      }]
+      expected_result = "date || credit || debit || balance\n01/01/2000 || 1000.0 || || 1000.0\n"
+      expect { StatementPrinter.run(statement) }.to output(expected_result).to_stdout
     end
   end
 end
