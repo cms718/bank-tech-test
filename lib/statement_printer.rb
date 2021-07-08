1658656dd8e7587b9491827744d39bad47fcfd4c
Charlie Slater <charliemslater1701@gmail.com>
2021-07-08 12:02:53 +0100

    Fixed typo

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
 class StatementPrinter
-  class << self
-    def run(transactions)
-      balance = 0
-      statement = []
-  
-      transactions.each do |transaction|
-        balance += transaction.value
-        if transaction.value > 0
-          statement << "#{transaction.created_at.strftime('%d/%m/%Y')} || #{transaction.value} || || #{balance}\n"
-        else
-          statement << "#{transaction.created_at.strftime('%d/%m/%Y')} || || #{transaction.value.abs} || #{balance}\n"
-        end
+  def self.run(transactions)
+    balance = 0
+    statement = []
+
+    transactions.each do |transaction|
+      balance += transaction.value
+      if transaction.value > 0
+        statement << "#{transaction.created_at.strftime('%d/%m/%Y')} || #{transaction.value} || || #{balance}\n"
+      else
+        statement << "#{transaction.created_at.strftime('%d/%m/%Y')} || || #{transaction.value.abs} || #{balance}\n"
       end
-      
-      puts 'date || credit || debit || balance'
-      puts statement.reverse.join if statement.any?
     end
+
+    puts 'date || credit || debit || balance'
+    puts statement.reverse.join if statement.any?
   end
 end
